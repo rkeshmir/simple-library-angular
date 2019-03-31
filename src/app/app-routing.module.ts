f1c7ffd9b8332f1407d78b67c36b1f8a68b03314
rkeshmir@yahoo.com <autumn2014>
2019-03-31 20:22:36 +0430

    not found module bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HeroesComponent } from './heroes/heroes.component';
 import {HomeComponent} from './home/home.component';
 import {ShelvesComponent} from './shelves/shelves.component';
 import {ShelfComponent} from './shelf/shelf.component';
