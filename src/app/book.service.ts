569fa0e7c67d9aca14e6e212a8244d9e41d95288
rkeshmir@yahoo.com <autumn2014>
2019-04-01 21:30:03 +0200

    remove excessive comments

--- src/app/book.service.ts
+++ src/app/book.service.ts
@@ -95,12 +95,6 @@ export class BookService {
       );
   }
 
-  /**
-   * Handle Http operation that failed.
-   * Let the app continue.
-   * @param operation - name of the operation that failed
-   * @param result - optional value to return as the observable result
-   */
   private handleError<T> (operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
       console.error(error); // log to console instead
